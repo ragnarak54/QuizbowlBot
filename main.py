7038693391dbcf407487a4f36e078f726021828a
Colton Sowers <cdsowers@asu.edu>
2018-08-05 16:32:08 -0700

    allow buzzing mid-question

--- main.py
+++ main.py
@@ -22,12 +22,24 @@ async def ping():
     await bot.say("pong!")
 
 
+@bot.command()
+async def buzz():
+    await asyncio.sleep(10)
+    print("buzz!")
+
+
 @bot.command(pass_context=True)
-async def question(ctx):
+async def question(ctx, num=1):
     correct = False
     user = ctx.message.author
-    while not correct:
+    wrong_buzzers = []
+    for j in range(0, num):
+
         question_obj = random.choice(questionlist)
+
+        # reading_task = bot.loop.create_task(read_question(question_obj))
+
+        # loop.run_until_complete(asyncio.ensure_future(read_question(question_obj), loop=loop))
         question_arr = question_obj.question.split(" ")
         sent_question = await bot.say(" ".join(question_arr[:5]))
         await asyncio.sleep(1)
@@ -35,18 +47,48 @@ async def question(ctx):
             sent_question_content = sent_question.content
             sent_question = await bot.edit_message(sent_question, sent_question_content + " " + " ".join(question_arr[i*5:i*5+5]))
             print(sent_question.content)
-            await asyncio.sleep(1)
 
-        msg = await bot.wait_for_message(author=user)
-        print(question_obj.question)
-        ratio = fuzz.ratio(msg.content.lower(), question_obj.answer.lower())
-        if ratio > 80:
-            await bot.say("correct!")
-            print("correct! ratio: " + str(ratio))
-            correct = True
-        else:
-            await bot.say("incorrect :(\nIt was " + question_obj.answer + "!")
-            print("incorrect")
+            def check(message):
+                return message.author not in wrong_buzzers and "buzz" in message.content
+
+            msg = await bot.wait_for_message(timeout=1, check=check)
+            if msg is not None:
+                await bot.say("buzz from {0}! 10 seconds to answer".format(msg.author))
+                answer = await bot.wait_for_message(timeout=10, author=msg.author)
+                ratio = fuzz.ratio(answer.content.lower(), question_obj.answer.lower())
+                if ratio > 80:
+                    await bot.say("correct!")
+                    print("correct! ratio: " + str(ratio))
+                    correct = True
+                    msg = None
+                    break
+                else:
+                    await bot.say("incorrect!")
+                    if answer.author not in wrong_buzzers:
+                        wrong_buzzers.append(answer.author)
+                    print("incorrect")
+                    msg = None
+
+        msg = await bot.wait_for_message(timeout=10, check=check)
+        if msg is not None:
+            await bot.say("buzz from {0}! 10 seconds to answer".format(msg.author))
+            answer = await bot.wait_for_message(timeout=10, author=msg.author)
+            ratio = fuzz.ratio(answer.content.lower(), question_obj.answer.lower())
+            if ratio > 80:
+                await bot.say("correct!")
+                print("correct! ratio: " + str(ratio))
+                correct = True
+            else:
+                await bot.say("incorrect!")
+                print("incorrect")
+        if not correct:
+            await bot.say("The answer is {0}!".format(question_obj.answer))
+
+        msg = None
+        msg = await bot.wait_for_message(timeout=20, content="next")
+        if msg is None:
+            return
+        wrong_buzzers.clear()
         print(question_obj.answer)
         print(question_obj.packet)
 
