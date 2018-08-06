3ea39865b4ca411794eac3cdb1ad9d5a1300fc70
Colton Sowers <cdsowers@asu.edu>
2018-08-05 22:04:07 -0700

    allow skipping questions and reprints question progress after wrong answer

--- main.py
+++ main.py
@@ -10,7 +10,6 @@ questionlist = packet_handling.get_questions()
 
 @bot.event
 async def on_ready():
-
     print('Logged in as')
     print(bot.user.name)
     print(bot.user.id)
@@ -22,15 +21,10 @@ async def ping():
     await bot.say("pong!")
 
 
-@bot.command()
-async def buzz():
-    await asyncio.sleep(10)
-    print("buzz!")
-
-
 @bot.command(pass_context=True)
 async def question(ctx, num=1):
     correct = False
+    skip = False
     user = ctx.message.author
     wrong_buzzers = []
     for j in range(0, num):
@@ -50,8 +44,33 @@ async def question(ctx, num=1):
 
             def check(message):
                 return message.author not in wrong_buzzers and "buzz" in message.content
+            msg = None
+            msg = await bot.wait_for_message(timeout=1)
+            if msg is not None:
+                if "buzz" in msg.content:
+                    await bot.say("buzz from {0}! 10 seconds to answer".format(msg.author))
+                    answer = await bot.wait_for_message(timeout=10, author=msg.author)
+                    ratio = fuzz.ratio(answer.content.lower(), question_obj.answer.lower())
+                    if ratio > 80:
+                        await bot.say("correct!")
+                        print("correct! ratio: " + str(ratio))
+                        correct = True
+                        msg = None
+                        break
+                    else:
+                        await bot.say("incorrect!")
+                        if answer.author not in wrong_buzzers:
+                            wrong_buzzers.append(answer.author)
+                        print("incorrect")
+                        msg = None
+                        sent_question = await bot.say(sent_question_content)
+                elif "skip" in msg.content:
+                    skip = True
+                    print("skip!")
+                    break
 
-            msg = await bot.wait_for_message(timeout=1, check=check)
+        if not skip:
+            msg = await bot.wait_for_message(timeout=10, check=check)
             if msg is not None:
                 await bot.say("buzz from {0}! 10 seconds to answer".format(msg.author))
                 answer = await bot.wait_for_message(timeout=10, author=msg.author)
@@ -60,34 +79,16 @@ async def question(ctx, num=1):
                     await bot.say("correct!")
                     print("correct! ratio: " + str(ratio))
                     correct = True
-                    msg = None
-                    break
                 else:
                     await bot.say("incorrect!")
-                    if answer.author not in wrong_buzzers:
-                        wrong_buzzers.append(answer.author)
                     print("incorrect")
-                    msg = None
+            if not correct:
+                await bot.say("The answer is {0}!".format(question_obj.answer))
+            msg = None
+            msg = await bot.wait_for_message(timeout=20, content="next")
+            if msg is None:
+                return
 
-        msg = await bot.wait_for_message(timeout=10, check=check)
-        if msg is not None:
-            await bot.say("buzz from {0}! 10 seconds to answer".format(msg.author))
-            answer = await bot.wait_for_message(timeout=10, author=msg.author)
-            ratio = fuzz.ratio(answer.content.lower(), question_obj.answer.lower())
-            if ratio > 80:
-                await bot.say("correct!")
-                print("correct! ratio: " + str(ratio))
-                correct = True
-            else:
-                await bot.say("incorrect!")
-                print("incorrect")
-        if not correct:
-            await bot.say("The answer is {0}!".format(question_obj.answer))
-
-        msg = None
-        msg = await bot.wait_for_message(timeout=20, content="next")
-        if msg is None:
-            return
         wrong_buzzers.clear()
         print(question_obj.answer)
         print(question_obj.packet)
