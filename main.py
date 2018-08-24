945627f6e56fb560cbd5ca40869e4c639c910a58
Colton Sowers <cdsowers@asu.edu>
2018-08-24 14:26:38 -0700

    big boi commit, !tournament base functionality

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import random
 from fuzzywuzzy import fuzz
 import asyncio
 import config
+from typing import List
 
 bot = commands.Bot(command_prefix=['!', '?'], description="Quiz bowl bot!")
 questionlist = packet_handling.get_questions()
@@ -32,12 +33,13 @@ class Group:
 
 
 class Team:
-    def __init__(self, server, name, captain, members):
+    def __init__(self, server, name, captain, members, score=0):
         # self.group = group
         self.server = server
         self.name = name
         self.captain = captain
         self.members = members
+        self.score = score
 
 
 class Player:
@@ -73,11 +75,22 @@ def get_group(member):
     return None
 
 
-def get_team(member):
+def get_team(member, server):
     for team in teams:
-        if member in team.members:
+        if member in team.members and team.server == server:
             return team
     return None
+
+
+def get_player(member, team):
+    for player in team.members:
+        if member == player:
+            return player
+
+def serialize_team(teamname, server):
+    for team in teams:
+        if team.name == teamname and team.server == server:
+            return team
 
 
 @bot.command(name="team", pass_context=True, aliases=["maketeam", "newteam"])
@@ -89,7 +102,7 @@ async def team_(ctx, name):
     if found_group is None:
         await bot.say("You must be in a group to create a team!")
         return'''
-    if get_team(ctx.message.author) is not None:
+    if get_team(ctx.message.author, ctx.message.server) is not None:
         await bot.say("You're already in a team.")
         return
     teams.append(Team(ctx.message.server, name, ctx.message.author, [ctx.message.author]))
@@ -109,7 +122,7 @@ async def teams_(ctx):
 
 @bot.command(pass_context=True)
 async def myteam(ctx):
-    team = get_team(ctx.message.author)
+    team = get_team(ctx.message.author, ctx.message.server)
     if team is not None:
         await bot.say(team.name)
     else:
@@ -118,7 +131,7 @@ async def myteam(ctx):
 
 @bot.command(name="captain", pass_context=True)
 async def captain_(ctx, new_captain: discord.Member=None):
-    team = get_team(ctx.message.author)
+    team = get_team(ctx.message.author, ctx.message.server)
     if team is not None:
         if new_captain is None:
             await bot.say("{0} is the captain of your team, {1}".format(team.captain, team.name))
@@ -147,7 +160,7 @@ async def join(ctx, name):
 
 @bot.command(pass_context=True, aliases=['leaveteam'])
 async def leave(ctx, name):
-    member_team = get_team(ctx.message.author)
+    member_team = get_team(ctx.message.author, ctx.message.server)
     if member_team is not None and member_team.name == name:
         if len(member_team.members) == 1:
             await bot.say("You're the last person in the team!\nLeaving the team has deleted it. You can always create"
@@ -167,13 +180,157 @@ async def leave(ctx, name):
 
 
 @bot.command(pass_context=True)
-async def tournament(ctx):
-    await bot.say("")
+async def tournament(ctx, *, teams_in_game=None):
+    caller_team = get_team(ctx.message.author, ctx.message.server)
+    if caller_team is None:
+        await bot.say("You should make or join a team first.")
+        return
+    if caller_team.captain != ctx.message.author:
+        await bot.say("You can't start a tournament unless you're a team captain.")
+        return
+    if teams_in_game is None:
+        await bot.say("Please enter the teams that will be playing in this tournament,"
+                      "separated by spaces")
+        msg = await bot.wait_for_message(author=ctx.message.author)
+        team_names = msg.content.split(" ")
+    else:
+        team_names = teams_in_game.split(" ")
+    if len(team_names) < 2:
+        await bot.say("You must have at least 2 teams to have a tournament!")
+        return
+    teams_in_game = []
+    for teamname in team_names:
+        teams_in_game.append(serialize_team(teamname, ctx.message.server))
+    for team in teams_in_game:
+        if team not in teams:
+            await bot.say("{0} isn't a team. Make sure to check your spelling! Teams currently "
+                          "in this server are:\n".format(team)
+                          + "".join([":small_blue_diamond:" + t_.name + "\n" for t_ in teams]))
+            return
 
+    await bot.say("Would you like bonus questions? Answer with yes or no.")
+    msg = await bot.wait_for_message(author=ctx.message.author)
+    if msg.content.lower() in ["yes", "y", "ye", "yeet"]:
+        bonus = True
+        await bot.say("Bonus questions will be read.")
+    else:
+        bonus = False
+        await bot.say("Bonus questions will not be read.")
+    await bot.say("How many tossup questions do you want (default is 20)?")
+    msg = await bot.wait_for_message(author=ctx.message.author)
+    num_of_questions = int(msg.content)
+    await bot.say("Tournament starting! Your setup is as follows:\n"
+                  "Teams competing: " + "".join([":small_blue_diamond:" + t_.name + ", " for t_ in teams_in_game]) +
+                  "\nNumber of tossups: " + str(num_of_questions) +
+                  "\nBonus questions: " + str(bonus) +
+                  "\nIf this is correct, type yes. If you'd like to edit something, type teams,tossups, or bonuses.")
 
-async def read_question():
+    def check2(message):
+        return message.content in ["yes", "y", "ye", "yeet", "teams", "tossups", "bonuses"]
+
+    msg = await bot.wait_for_message(author=ctx.message.author, check=check2)
+    if msg.content.lower() in ["yes", "y", "ye", "yeet"]:
+        await bot.say("Tournament starting! Good luck!")
+    # TODO
+    elif msg.content.lower() == "teams":
+        await bot.say("Alright, re-enter the list of teams competing, separated by spaces")
+        msg = await bot.wait_for_message(author=ctx.message.author)
+        teams_in_game = msg.content.split(" ")
+        if teams_in_game.count() < 2:
+            await bot.say("You must have at least 2 teams to have a tournament!")
+            return
+        for team in teams_in_game:
+            if team not in [t.name for t in teams]:
+                await bot.say("{0} isn't a team. Make sure to check your spelling! Teams currently "
+                              "in this server are:\n".format(team)
+                              + "".join([":small_blue_diamond:" + t_.name + "\n" for t_ in teams]))
+                return
+    elif msg.content.lower() == "tossups":
+        await bot.say("Alright, re-enter the number of tossups you want.")
+        msg = await bot.wait_for_message(author=ctx.message.author)
+        num_of_questions = int(msg.content)
+    elif msg.content.lower() == "bonuses":
+        await bot.say("Alright, re-renter yes or no if you want bonuses or not.")
+        msg = await bot.wait_for_message(author=ctx.message.author)
+        if msg.content.lower() in ["yes", "y", "ye", "yeet"]:
+            bonus = True
+            await bot.say("Bonus questions will be read.")
+        else:
+            bonus = False
+            await bot.say("Bonus questions will not be read.")
+    players = []
+    for t in teams_in_game:
+        print(t.members)
+        players += t.members
+    print(players)
+    for i in range(num_of_questions):
+        await read_question(bonus, players)
+    teams_in_game.sort(key=lambda x: x.score, reversed=True)
+    await bot.say("Tournament over! Final leaderboard:\n" +
+                  "".join([":small_blue_diamond:" + t.name + ": " + t.score + " points!\n" for t in teams_in_game]))
+    
+
+async def read_question(bonus: bool, players):
     correct = False
     skip = False
+    neggers = []
+    question_obj = random.choice(questionlist)
+    question_arr = question_obj.question.split(" ")
+    sent_question = await bot.say(" ".join(question_arr[:5]))
+    await asyncio.sleep(1)
+    for i in range(1, question_arr.__len__() // 5 + 1):
+        sent_question_content = sent_question.content
+        sent_question = await bot.edit_message(sent_question,
+                                               sent_question_content + " " + " ".join(question_arr[i * 5:i * 5 + 5]))
+        print(sent_question.content)
+
+        def check(message):
+            return message.author in players and message.author not in neggers and "buzz" in message.content
+
+        msg = None
+        msg = await bot.wait_for_message(timeout=1, check=check)
+        if msg is not None:
+            await bot.say("buzz from {0}! 10 seconds to answer".format(msg.author))
+            answer = await bot.wait_for_message(timeout=10, author=msg.author)
+            ratio = fuzz.ratio(answer.content.lower(), question_obj.answer.lower())
+            if ratio > 75:
+                await bot.say("correct!")
+                print("correct! ratio: " + str(ratio))
+                correct = True
+                await bot.edit_message(sent_question, question_obj.question)
+                team = get_team(msg.author, msg.author.server)
+                player = get_player(msg.author, team)
+                team.score += 20
+                player.score += 20
+                break
+            else:
+                await bot.say("incorrect!")
+                print("incorrect")
+                msg = None
+                sent_question = await bot.say(sent_question_content)
+    if not correct:
+        msg = None
+        msg = await bot.wait_for_message(timeout=10, check=check)
+        if msg is not None:
+            await bot.say("buzz from {0}! 10 seconds to answer".format(msg.author))
+            answer = await bot.wait_for_message(timeout=10, author=msg.author)
+            ratio = fuzz.ratio(answer.content.lower(), question_obj.answer.lower())
+            if ratio > 75:
+                await bot.say("correct!")
+                print("correct! ratio: " + str(ratio))
+                correct = True
+                team = get_team(msg.author, msg.author.server)
+                player = get_player(msg.author, team)
+                team.score += 20
+                player.score += 20
+            else:
+                await bot.say("incorrect!")
+                print("incorrect")
+
+        if not correct:
+            await bot.say("The answer is {0}!".format(question_obj.answer))
+
+    neggers.clear()
 
 
 @bot.command(pass_context=True)
