31f366d30079a7abe515785ffd0621d27632e991
Colton Sowers <cdsowers@asu.edu>
2018-11-12 00:20:15 -0700

    change question class

--- question.py
+++ question.py
@@ -1,6 +1,11 @@
 class Question:
-    def __init__(self, question, answer, packet):
+    def __init__(self, question, answer, category, packet, formatted_question=None, formatted_answer=None):
         self.question = question
         self.answer = answer
+        self.category = category
         self.packet = packet
+        self.formatted_question = formatted_question
+        self.formatted_answer = formatted_answer
+
+
 
